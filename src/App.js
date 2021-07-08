17650bb3aa05c20aa3c5371f91bb6fc371c5b035
Shreya Basnet <basnet.shreyaa@gmail.com>
2021-07-08 14:06:15 +0545

    delete working

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
+import { filter } from "minimatch";
 import React, { useState } from "react";
 import "./App.css";
 import AddList from "./Components/ToDo/AddList";
 import DisplayList from "./Components/ToDo/DisplayList";
+import Btnclass from "./Components/UI/Button.module.css";
 
 function App() {
   const [toDoList, setToDoList] = useState([]);
- 
 
-  const addToDoListHandler = (todo,checked) => {
+  const addToDoListHandler = (todo, checked) => {
     setToDoList((prevList) => {
       return [
         ...prevList,
         {
           todo: todo,
-          priority:checked,
+          priority: checked,
           id: Math.random().toString(),
+          complete: false,
         },
       ];
     });
   };
-  const deleteHandler = (toDoId)=>{
-  const newTodos = toDoList.filter((_, id) => id !== toDoId);
+  const deleteHandler = (toDoId) => {
+    const newTodos = toDoList.filter((i) => i.id !== toDoId);
 
-  setToDoList(newTodos);
+    setToDoList(newTodos);
+  };
+  const doneHandler = (toDoId) => {
+    let mapped = toDoList.map((task) => {
+      return task.id === toDoId
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
 
-
-  }
- 
+    setToDoList(mapped);
+  };
+  const filterHandler = () => {
+    let filtered = toDoList.filter((task) => task.priority === true);
+    setToDoList(filtered);
+    console.log(filtered);
+  };
 
   return (
     <div>
       <AddList onAddTodo={addToDoListHandler} />
-      <DisplayList toDoList={toDoList} onDelete={deleteHandler}/>
+      <button className={Btnclass.button} onClick={filterHandler}>
+        {" "}
+        Show only Priority{" "}
+      </button>
+
+      <DisplayList
+        toDoList={toDoList}
+        onDone={(id) => doneHandler(id)}
+        delete={(id) => deleteHandler(id)}
+      />
     </div>
   );
 }
--- src/Components/ToDo/DisplayList.js
+++ src/Components/ToDo/DisplayList.js
@@ -1,36 +1,31 @@
-
 import React, { useState } from "react";
 import "./DisplayList.css";
 import Cards from "../UI/Cards";
 import Button from "../UI/Button";
-import btnClass from '../UI/Button.module.css'
-
+import btnClass from "../UI/Button.module.css";
 
 const DisplayList = (props) => {
-    const [isDone,setIsDone] = useState(false)
-    const[isDeleted,setIsDeleted] =useState(false)
-      const deleteHandler = (props) => {
-        // const id = props.toDoList[id]
-   props.onDelete()
-      };
-     
-      if(props.toDoList.length ===0 ){
-          return <Cards className='users'>
-              Nothing to see here
-          </Cards>
-      }
+  if (props.toDoList.length === 0) {
+    return <Cards className="users">Nothing to see here</Cards>;
+  }
+  
   return (
-    <Cards className={`users ${isDone ? "done" : ""}`}>
+    <Cards className="users">
       <ul>
-        {props.toDoList.map((toDoItem) => (
-          <li key={toDoItem.id}>
-            {toDoItem.todo}
-            <button className={btnClass.button} >
+        {props.toDoList.map(({todo, id,complete}) => (
+          <li key={id}>
+             <span className={complete ? "strike" : ""}>
+            {todo}
+            </span>
+            <button
+              className={btnClass.button}
+              onClick={() => props.onDone(id)}
+            >
               Done
             </button>
             <button
               className={btnClass.button}
-              onClick={deleteHandler}
+              onClick={() => props.delete(id)}
             >
               Delete{" "}
             </button>
